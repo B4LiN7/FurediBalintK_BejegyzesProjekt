5fed0cbafe2b94aafba782212baa6867383f383c
B4LiN7 <balint.k.furedi@outlook.com>
2023-09-26 08:03:53 +0200

    Fájlok feltöltése

--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BejegyzesProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
